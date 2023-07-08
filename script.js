8a48d4397f217fb09b1e1fcd4f50c5c4f74000e4
Aminah Cherry <leeocherry@Lees-MacBook-Pro.local>
2023-07-07 20:49:14 -0700

    starterindex and java code

---
+++ script.js
@@ -0,0 +1,25 @@
+var myquestions = [
+    {
+        question: "Commonly Used Data Types Include",
+        answers: {
+            a: 'strings',
+            b: 'booleans'
+        }
+
+
+    }
+
+]
+
+
+
+function generatequiz {
+    //code
+
+
+    function show questions {
+        //code
+
+    }
+
+}
